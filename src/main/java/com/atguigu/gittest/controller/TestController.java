18a0b70fb6ee6748be1ebf96fc88949140d5a623
mks_glb <572733154@qq.com>
2020-02-12 19:54:30 +0800

    修改代码

---
+++ src/main/java/com/atguigu/gittest/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.atguigu.gittest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello(){
+        return "你好";
+    }
+
+}
